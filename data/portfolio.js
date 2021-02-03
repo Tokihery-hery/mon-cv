58b49b7bb5604f8cac2808d834205e2af395291b
Tokihery-hery <74452123+Tokihery-hery@users.noreply.github.com>
2021-02-03 16:22:05 +0300

    Update portfolio.js

--- data/portfolio.js
+++ data/portfolio.js
@@ -3,64 +3,64 @@ let porfolio = [
     {
         "id": 1,
         "description": "Dessiner Android / Angular/Node",
-        "gitRepository": "http://android-designer.git",
-        "gitPage": "http://git.page1",
+        "gitRepository": "https://github.com/Tokihery-hery/angular.git",
+        "gitPage": "non disponnible",
         "images": "./image/DESIGN android.JPG"
     },
     {
         "id": 2,
         "description": "Dessiné un modem/Html & Css ",
-        "gitRepository": "http://dessine-modem.git",
-        "gitPage": "http://git.page2",
+        "gitRepository": "https://github.com/Tokihery-hery/modem-designer.git",
+        "gitPage": " https://tokihery-hery.github.io/modem-designer/",
         "images": "./image/modem ouvert leru pouche rightn.JPG"
     },
     {
         "id": 3,
         "description": "Angular dev front/ Node Back end",
-        "gitRepository": "http://angular-dev.git",
-        "gitPage": "http://git.page3",
+        "gitRepository": " ",
+        "gitPage": " ",
         "images": "./image/emora 1.JPG"
     },
      {
         "id": 4,
         "description": "Laravel crud && Laravel Rest api",
-        "gitRepository": "http://laravel-crud.git",
-        "gitPage": "http://git.page4",
+        "gitRepository": "https://github.com/Tokihery-hery/tLaravel.git",
+        "gitPage": "NON DISPONNIBLE NOW ENCOURS DE 'DEPLOYED'",
         "images": "./image/laravel_crud.JPG"
     },
     {
         "id": 5,
         "description": "Emora project /Angular && Nodejs encours ...",
-        "gitRepository": "http://emora.git",
-        "gitPage": "http://git.page5",
+        "gitRepository": "https://github.com/Tokihery-hery/tEmora.git",
+        "gitPage": "non DISPONNIBLE NOW",
         "images": "./image/HOME EMORA.JPG"
     },
     {
         "id": 6,
         "description": "Node Rest api / avec Expressjs",
-        "gitRepository": "http://emora.git",
-        "gitPage": "http://git.page5",
+        "gitRepository": "https://github.com/Tokihery-hery/tEmora.git",
+        "gitPage": "non DISPONNIBLE NOW",
         "images": "./image/HOME EMORA.JPG"
     },
     {
         "id": 7,
         "description": "Apprendre Jquery",
-        "gitRepository": "http://emora.git",
-        "gitPage": "http://git.page5",
+        "gitRepository": "https://github.com/Tokihery-hery/tJquery.git",
+        "gitPage": " https://tokihery-hery.github.io/tJquery/",
         "images": "./image/Jquery copy coller.JPG"
     },
     {
         "id": 8,
         "description": "API REST node js,express js avec Mongodb",
         "gitRepository": "http://emora.git",
-        "gitPage": "http://git.page5",
+        "gitPage": "localhost disponnible",
         "images": "./image/node crud mongo.JPG"
     },
     {
         "id": 9,
         "description": "API REST node js, express js avec MYSQL",
-        "gitRepository": "http://emora.git",
-        "gitPage": "http://git.page5",
+        "gitRepository": "",
+        "gitPage": "localhost disponnible",
         "images": "./image/api node msql.JPG"
     },
     
